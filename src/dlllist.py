a5229d0c8d0c9b4fc6c2db6109656ee81decdc3e
Shane <shane@swelcher.com>
2018-06-08 22:11:53 -0700

    updated gitignore

---
+++ src/dlllist.py
@@ -0,0 +1,20 @@
+from src.jsonpost import jsonpost
+
+
+def dlllist(json_file, vol_host, url):
+
+        process_id = []
+
+        with open(json_file) as file:
+                        vol_file = json.load(file)
+                        for row in vol_file['rows']:
+                                dictionary = {}
+                                for key, value in zip(vol_file['columns'], row):
+                                        dictionary[key] = value
+
+                                pid = enumerate_dict_pid(dictionary)
+                                if pid not in process_id:
+                                        process_id.append(pid)
+                                        dictionary["plugin"] = "dlllist"
+                                        jsonpost(vol_host, url, dictionary)
+
