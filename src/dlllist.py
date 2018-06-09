fb49876eb02cf482b04ae0ece067204810f27d3b
Shane <shane@swelcher.com>
2018-06-09 07:11:31 -0700

    dlllist

--- src/dlllist.py
+++ src/dlllist.py
@@ -5,28 +5,28 @@ from src.compare import enumerate_dict_name
 
 def dlllist(json_file, vol_host, url):
 
-        process_id = []
+	process_id = []
 	
 
 	# Initial Analysis of JSON File
-        with open(json_file) as file:
-                vol_file = json.load(file)
-                for row in vol_file['rows']:
-                        analyzedictionary = {}
-                        for key, value in zip(vol_file['columns'], row):
-                                analyzedictionary[key] = value
+	with open(json_file) as file:
+		vol_file = json.load(file)
+		for row in vol_file['rows']:
+			analyzedictionary = {}
+			for key, value in zip(vol_file['columns'], row):
+				analyzedictionary[key] = value
             # Call to enumerate_dict Function. Located in src.
-                        process_name = enumerate_dict(analyzedictionary).lower()
+			process_name = enumerate_dict(analyzedictionary).lower()
 
-        with open(json_file) as file:
-                        vol_file = json.load(file)
-                        for row in vol_file['rows']:
-                                dictionary = {}
-                                for key, value in zip(vol_file['columns'], row):
-                                        dictionary[key] = value
+	with open(json_file) as file:
+			vol_file = json.load(file)
+			for row in vol_file['rows']:
+				dictionary = {}
+				for key, value in zip(vol_file['columns'], row):
+					dictionary[key] = value
 
-                                pid = enumerate_dict_pid(dictionary)
-                                if pid not in process_id:
+				pid = enumerate_dict_pid(dictionary)
+				if pid not in process_id:
 					process_id.append(pid)
 					dictionary["Executable"] = "True"
 					dictionary["plugin"] = "dlllist"
